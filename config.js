ea83609658cab4acaa5f0e3132c877b0e56074cd
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-04-11 18:47:52 +0200

    config and .env-example

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
   port: 3000,
-  storageBucketName: '',
-  keyFilename: '',
-  projectId: '',
+  storageBucketName: process.env.STORAGE_BUCKET_NAME,
+  keyFilename: process.env.KEY_FILE_NAME,
+  projectId: process.env.PROJECT_ID,
   DBAuth: process.env.DB_AUTH,
   DBUrl: process.env.DB_URI,
 };
