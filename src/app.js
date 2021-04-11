d01fd450e45ae38dfb04ee1515a8f0937b8aaeeb
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-04-11 19:00:38 +0200

    add rate limit on app request

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const xss = require('xss-clean');
+const rateLimit = require('express-rate-limit');
 const { loggerMiddleware, errorHandlerMiddleware: { notFound, handleError } } = require('./middleware');
 
 const corsOpts = { origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'] };
@@ -19,6 +20,15 @@ app.use(loggerMiddleware);
 app.use(bodyParser.json({ limit: '200mb' }));
 app.use(bodyParser.urlencoded({ limit: '200mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
+
+app.set('trust proxy', 1);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+//  apply to all requests
+app.use(limiter);
 
 // App main router prefixed by /apis as our current configuration forwards requests with prefix /apis to this app
 const mainRouter = new express.Router();
