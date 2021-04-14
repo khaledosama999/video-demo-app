381c2270743b8f0464b78b54e56a899905156ce7
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-04-14 15:45:24 +0200

    serve front-end

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const xss = require('xss-clean');
 const rateLimit = require('express-rate-limit');
+const path = require('path');
 const { loggerMiddleware, errorHandlerMiddleware: { notFound, handleError } } = require('./middleware');
 
 const corsOpts = { origin: '*', methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'] };
@@ -20,6 +21,7 @@ app.use(loggerMiddleware);
 app.use(bodyParser.json({ limit: '200mb' }));
 app.use(bodyParser.urlencoded({ limit: '200mb', extended: true, parameterLimit: 50000 }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/front-end/build')));
 
 app.set('trust proxy', 1);
 const limiter = rateLimit({
@@ -36,6 +38,10 @@ app.use('/apis', mainRouter);
 
 mainRouter.use('/healthcheck', require('./routes/healthcheck'));
 require('./routes/video')(mainRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../front-end/build', 'index.html'));
+});
 
 app.use(handleError);
 app.use(notFound);
