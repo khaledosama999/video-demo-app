601c3f1a40aea21bb4f95d0ce6501de1ba5b077a
Milad E. Fahmy <miladezzat.f@gmail.com>
2021-04-14 14:58:58 +0200

    setup front end

---
+++ front-end/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+  Container,
+} from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import NavBar from './components/nav';
+import DisplayVideo from './sections/display';
+import UploadSection from './sections/upload';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Container fluid>
+        <Switch>
+          <Route path="/upload"><UploadSection /></Route>
+          <Route path="/home">
+            <DisplayVideo />
+          </Route>
+          <Route path="/video">
+            <div className="player-wrapper">
+              <ReactPlayer
+                className="react-player"
+                controls
+                url="https://www.youtube.com/watch?v=ysz5S6PUM-U"
+                width="100%"
+                height="100%"
+              />
+            </div>
+          </Route>
+        </Switch>
+      </Container>
+    </>
+  );
+}
+
+export default App;
